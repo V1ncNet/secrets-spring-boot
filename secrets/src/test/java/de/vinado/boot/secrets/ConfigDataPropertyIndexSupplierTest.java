a4f50ba2eafdb8878395089f312b35e4357303bc
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-04-12 14:11:17 +0200

    Refactor config data supplier test

--- secrets/src/test/java/de/vinado/boot/secrets/ConfigDataPropertyIndexSupplierTest.java
+++ secrets/src/test/java/de/vinado/boot/secrets/ConfigDataPropertyIndexSupplierTest.java
@@ -6,12 +6,11 @@ import org.springframework.boot.DefaultPropertiesPropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.StandardEnvironment;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
+import static de.vinado.boot.secrets.TestUtils.fileUriFromClasspath;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -30,10 +29,21 @@ class ConfigDataPropertyIndexSupplierTest {
     }
 
     @Test
-    void nonApplicableConfig_soundNotPostProcessEnvironment() {
+    void otherConfigData_shouldNotBeIndexed() {
         Map<String, Object> applicationProperties = new HashMap<>();
         applicationProperties.put("spring.mail.host", "foo");
         applicationProperties.put("spring.datasource.username", "bob");
+        setUpSupplier(applicationProperties);
+
+        Map<String, String> index = supplier.get();
+
+        assertNotNull(index);
+        assertEquals(0, index.size());
+    }
+
+    @Test
+    void malformedConfigData_shouldNotBeIndexed() {
+        Map<String, Object> applicationProperties = new HashMap<>();
         applicationProperties.put("secrets.file.properties", "classpath:spring_mail_host");
         setUpSupplier(applicationProperties);
 
@@ -44,12 +54,10 @@ class ConfigDataPropertyIndexSupplierTest {
     }
 
     @Test
-    void applicationConfig_shouldPostProcessEnvironment() {
+    void prefixedConfigData_shouldBeIndexed() {
         Map<String, Object> applicationProperties = new HashMap<>();
-        applicationProperties.put("spring.mail.host", "foo");
-        applicationProperties.put("spring.datasource.username", "bob");
         applicationProperties.put("secrets.file.properties.spring.mail.host", "classpath:spring_mail_host");
-        applicationProperties.put("secrets.file.properties.secret.empty", fromFile("secret.empty"));
+        applicationProperties.put("secrets.file.properties.secret.empty", fileUriFromClasspath("secret.empty"));
         setUpSupplier(applicationProperties);
 
         Map<String, String> index = supplier.get();
@@ -61,18 +69,12 @@ class ConfigDataPropertyIndexSupplierTest {
         assertEquals("classpath:spring_mail_host", index.get("spring.mail.host"));
 
         assertTrue(index.containsKey("secret.empty"));
-        assertEquals(fromFile("secret.empty"), index.get("secret.empty"));
+        assertEquals(fileUriFromClasspath("secret.empty"), index.get("secret.empty"));
     }
 
     private void setUpSupplier(Map<String, Object> applicationProperties) {
         DefaultPropertiesPropertySource.addOrMerge(applicationProperties, environment.getPropertySources());
 
         supplier = new ConfigDataPropertyIndexSupplier(Supplier::get, environment, "file");
-    }
-
-    private static String fromFile(String name) {
-        String pathname = String.format("%s/src/test/resources/%s", System.getProperty("user.dir"), name);
-        Path path = Paths.get(pathname);
-        return path.toUri().toString();
     }
 }
